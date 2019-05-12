6ec79fc42d017884553301ccae01d97bb3c740a2
unknown <ubaidurrehman@10pearls.com>
2019-05-12 23:27:38 +0500

    support for 4.5

--- blog.AspNetReact/Scripts/Home/react/Index.js
+++ blog.AspNetReact/Scripts/Home/react/Index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 const App = () => (
     <Fragment>
         <h1>React works in ASP.NET MVC! Yes!</h1>
-        <div>Hello React World!!xxx!</div>
+        <div>Hello React World!!xxx! Hello Test! 2</div>
     </Fragment>
 );
 
